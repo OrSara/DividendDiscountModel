eefadd5fbc6851cd95a8f34279acef4c4220296b
OrSara <162731615+OrSara@users.noreply.github.com>
2024-05-10 13:49:19 +0100

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ app.config['MONGO_URI'] = MONGO_URI
 
 
 
-@app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index')
+    return render_template('index.html')
 
 
 @app.route('/riskFreeRate', methods=['POST'])
